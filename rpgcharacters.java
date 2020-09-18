f06a88f369773603e4a6af10af1a7d73c625875b
Dennis To <dnnst3638@gmail.com>
2020-09-18 15:00:31 -0700

    add rpgcharacters.java

---
+++ rpgcharacters.java
@@ -0,0 +1,11 @@
+public class rpgcharacters{
+
+    public static void main(String[] args) {
+        
+        warrior c1 = new warrior("Jim");
+        warrior c2 = new warrior("Bob");
+        warrior c3 = c1;
+        warrior c4 = new warrior("Rushil", 10);
+    }
+
+}
