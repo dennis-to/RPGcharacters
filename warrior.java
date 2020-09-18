647d6c3ec6e1da92cf5ae738b10c19a478b25cd4
Dennis To <dnnst3638@gmail.com>
2020-09-18 14:59:48 -0700

    add warrior.java

--- warrior.java
+++ warrior.java
@@ -1,3 +1,40 @@
 public class warrior {
+
+private String name;
+private int level;
+private int maxhealth;
+private int health;
+private int attack;
+private int magicattack;
+private int defense;
+private int magicdefense;
+private int agility;
+private int wisdom;
+
+    public warrior(String n){
+        name = n;
+        level = 1;
+        maxhealth = 20;
+        health = maxhealth;
+        attack = 10;
+        defense = 8;
+        magicattack = 0;
+        magicdefense = 4;
+        agility = 7;
+        wisdom = 2;
+    }
+    public warrior(String n, int lev){
+        name = n;
+        level = lev;
+        maxhealth = 20 * lev;
+        health = maxhealth;
+        attack = 10 * lev;
+        defense = 8 * lev;
+        magicattack = 0 * lev;
+        magicdefense = 4 * lev;
+        agility = 7 * lev;
+        wisdom = 2 * lev;
+
+    }
     
 }
